559770b1316c3c833338f2adde3c2fd445177a21
firewallfail <firewall.fail@hotmail.com>
2022-06-01 18:25:01 -0400

    Display categories at admin/categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -3,6 +3,7 @@ class Admin::CategoriesController < ApplicationController
   http_basic_authenticate_with name: ENV["ADMIN_USER_NAME"], password: ENV["ADMIN_PASSWORD"]
 
   def index
+    @categories = Category.order(id: :desc).all
   end
 
   def new
