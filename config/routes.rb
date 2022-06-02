da2dd544a18b6a3db628820d8d57058ca67a28f0
firewallfail <firewall.fail@hotmail.com>
2022-06-02 10:51:41 -0400

    Redirect to signup on failed signup

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :categories, only: [:show]
 
   resources :users, only: [:new, :create]
+  resources :session, only: [:new, :create, :destroy]
 
   resource :cart, only: [:show] do
     post   :add_item
