3c9f6631030add92c6aab166bfe4a3431fd18f99
firewallfail <firewall.fail@hotmail.com>
2022-06-06 14:42:16 -0400

    tests written

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,6 +2,29 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    # validation tests/examples here
+    it "succeeds if all fields are valid" do
+      @category = Category.new
+      @product = Product.create(name: "rspec", category: @category, price: 1, quantity: 1)
+      expect(@product.errors.full_messages).to be_empty
+    end
+    it "succeeds if name is blank" do
+      @category = Category.new
+      @product = Product.create(category: @category, price: 1, quantity: 1)
+      expect(@product.errors.full_messages).not_to be_empty
+    end
+    it "succeeds if price is blank" do
+      @category = Category.new
+      @product = Product.create(name: "rspec", category: @category, quantity: 1)
+      expect(@product.errors.full_messages).not_to be_empty
+    end
+    it "succeeds if quantity is blank" do
+      @category = Category.new
+      @product = Product.create(name: "rspec", category: @category, price: 1)
+      expect(@product.errors.full_messages).not_to be_empty
+    end
+    it "succeeds if category is blank" do
+      @product = Product.create(name: "rspec", price: 1, quantity: 1)
+      expect(@product.errors.full_messages).not_to be_empty
+    end
   end
 end
