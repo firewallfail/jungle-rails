1fbdafeb45e2bb6046893e3a2366849c9b6e8ac1
firewallfail <firewall.fail@hotmail.com>
2022-06-06 16:40:52 -0400

    validates form

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,13 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  def authenticate_with_credentials
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :password_digest, presence: true
+
+  def self.authenticate_with_credentials(email, password)
+    true
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  
+
+  describe 'Validations' do
+    it "succeeds if all fields are valid" do
+      @user = User.create(first_name: "Brent", last_name: "Hall", email: "test@test.com", password_digest: "password")
+      expect(@user.errors.full_messages).to be_empty
+    end
+    it "fails if first_name is blank" do
+      @user = User.create(last_name: "Hall", email: "test@test.com", password_digest: "password")
+      expect(@user.errors.full_messages).not_to be_empty
+    end
+    it "fails if last_name is blank" do
+      @user = User.create(first_name: "Brent", email: "test@test.com", password_digest: "password")
+      expect(@user.errors.full_messages).not_to be_empty
+    end
+    it "fails if email is blank" do
+      @user = User.create(first_name: "Brent", last_name: "Hall", password_digest: "password")
+      expect(@user.errors.full_messages).not_to be_empty
+    end
+    it "fails if password_digest is blank" do
+      @user = User.create(first_name: "Brent", last_name: "Hall", email: "test@test.com")
+      expect(@user.errors.full_messages).not_to be_empty
+    end
+  end
+
+  describe '.authenticate_with_credentials' do
+
+  end
+
 end
